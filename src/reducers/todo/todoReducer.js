32e753d4a0f4244c96cf5044106ebc37fd1c879d
Amrit Singh <amritsingh@innotechteam.in>
2023-11-28 18:28:37 +0530

    remove initialStateData

--- src/reducers/todo/todoReducer.js
+++ src/reducers/todo/todoReducer.js
@@ -1,10 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{
-        id: 1,
-        text: "Hello world"
-    }]
+    todos: []
 }
 
 export const todoReducer = createSlice({
